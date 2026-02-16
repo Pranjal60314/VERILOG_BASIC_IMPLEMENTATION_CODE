2d39a26ed8af7fc50922cae5b97f0e04f3f8d671
Pranjal Pandey <pythonofapollo0609@gmail.com>
2026-02-16 21:44:27 +0530

    Add files via upload

---
+++ half_adder_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ps
+`include "half_adder.v"
+
+module full_adder_tb;
+
+reg a, b, cin;
+wire sum, cout;
+
+full_adder uut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+);
+
+initial begin
+    $dumpfile(`VCD_FILE);
+    $dumpvars(0, full_adder_tb);
+
+    a=0; b=0; cin=0; #10;
+    a=0; b=0; cin=1; #10;
+    a=0; b=1; cin=0; #10;
+    a=0; b=1; cin=1; #10;
+    a=1; b=0; cin=0; #10;
+    a=1; b=0; cin=1; #10;
+    a=1; b=1; cin=0; #10;
+    a=1; b=1; cin=1; #10;
+
+    $finish;
+end
+
+endmodule
